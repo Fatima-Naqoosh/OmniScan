6e4aa24cdbeff05a59a8e5a2126c12d047bb52d3
Fatima-Naqoosh <naqooshfatima1999@gmail.com>
2021-10-21 15:41:49 +0530

    camera implementation

--- omniscan/lib/ocr_screen.dart
+++ omniscan/lib/ocr_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 class OCRScreen extends StatefulWidget {
   const OCRScreen({ Key? key }) : super(key: key);
 
@@ -20,4 +20,51 @@ class _OCRScreenState extends State<OCRScreen>  {
     );
   }
 
+} */
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class OCRScreen extends StatefulWidget {
+  final List<CameraDescription> cameras;
+
+  OCRScreen(this.cameras);
+
+  @override
+  _OCRScreenState createState() {
+    return new _OCRScreenState();
+  }
+}
+
+class _OCRScreenState extends State<OCRScreen> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        new CameraController(widget.cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return new Container();
+    }
+    return new AspectRatio(
+      aspectRatio: controller.value.aspectRatio,
+      child: new CameraPreview(controller),
+    );
+  }
 }
